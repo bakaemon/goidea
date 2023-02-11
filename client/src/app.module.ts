1f2f787c87f9ef5ec4f90bee1e5d8260f87ff5da
blackkillers20 <77771541+blackkillers20@users.noreply.github.com>
2023-02-11 19:36:40 +0700

    add new branch, add user and create user structure

--- client/src/app.module.ts
+++ client/src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './modules/roles/admin.module';
+import { UserModule } from './modules/features/users.module';
+import { CreateUsersModule } from './modules/features/CreateUsers.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AdminModule,
+    UserModule,
+    CreateUsersModule,
+    RouterModule.register([
+      {
+        path: 'admin',
+        module: AdminModule,
+        children: [
+          {
+          path: 'users',
+          module: UserModule,
+          },
+          {
+            path: 'createUsers',
+            module: CreateUsersModule,
+          }
+        ]
+      },
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
