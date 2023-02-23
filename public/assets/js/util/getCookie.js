d8edafd4093a1de5c80586a58c7ce771760133d3
blackkillers20 <77771541+blackkillers20@users.noreply.github.com>
2023-02-23 19:20:11 +0700

    minor changes

--- public/assets/js/util/getCookie.js
+++ public/assets/js/util/getCookie.js
@@ -3,3 +3,13 @@ function getCookie(name) {
     var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
     return match ? match[1] : null;
 }
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
