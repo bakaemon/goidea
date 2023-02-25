dc46ec148c23baec140973e67646c336b3d89e7b
bakaemon <69375806+bakaemon@users.noreply.github.com>
2023-02-25 22:15:04 +0700

    removed redudant module

--- src/modules/date/date.create.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DateCreateController } from './date.create.controller';
-
-@Module({
-    imports: [],
-    controllers: [DateCreateController],
-})
-export class DateCreateModule {}
