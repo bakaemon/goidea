d2b9dcf5f404db3706387e6caf65251e15ed4b79
bakaemon <69375806+bakaemon@users.noreply.github.com>
2023-03-14 07:05:52 +0700

    update layout directories

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -5,7 +5,7 @@ import Role from '@src/common/enums/role.enum';
 import RoleGuard from '@src/common/guards/role.guard';
 import { HttpAuthFilter } from '@common/filters/http-unauthorize-filter';
 import HttpRoleGuard from '@src/common/guards/http-role.guard';
-
+const mainLayout = "dashboard/main"
 @Controller()
 @UseFilters(HttpAuthFilter)
 @UseGuards(HttpRoleGuard(Role.Admin))
@@ -14,32 +14,32 @@ export class AdminController {
 
   @Get()
   root(@Res() res: Response) {
-    return res.render('dashboard/roles/admin/dashboard', { layout: 'main' });
+    return res.render('dashboard/roles/admin/dashboard', { layout: mainLayout });
   }
 
   @Get('users')
   users(@Res() res: Response) {
-    return res.render('dashboard/roles/admin/user_index', { layout: 'main' });
+    return res.render('dashboard/roles/admin/user_index', { layout: mainLayout });
   }
 
   @Get('users/create')
   createUsers(@Res() res: Response) {
-    return res.render('dashboard/roles/admin/CreateUsers_index', { layout: 'main' });
+    return res.render('dashboard/roles/admin/CreateUsers_index', { layout: mainLayout });
   }
 
   @Get('departments')
   Organization(@Res() res: Response) {
-    return res.render('dashboard/departments/dashboard', { layout: 'main' });
+    return res.render('dashboard/departments/dashboard', { layout: mainLayout });
   }
 
   @Get('departments/create')
   createOrganization(@Res() res: Response) {
-    return res.render('dashboard/departments/departments_create', { layout: 'main' });
+    return res.render('dashboard/departments/departments_create', { layout: mainLayout });
   }
 
   @Get('events')
   events(@Res() res: Response) {
-    return res.render('dashboard/events/dashboard', { layout: 'main' });
+    return res.render('dashboard/events/dashboard', { layout: mainLayout });
   }
   
 }
