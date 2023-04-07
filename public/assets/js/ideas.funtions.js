17cfe242107dd5e492605f2396c86b2de50d625c
blackkillers20 <blackkillers20@gmail.com>
2023-04-07 21:42:08 +0700

    edit ideas (staff) WIP

--- public/assets/js/ideas.funtions.js
+++
@@ -1,8 +0,0 @@
-async function editIdeas() {
-     window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-            selectedid = null;
-        }
-    }
-}
